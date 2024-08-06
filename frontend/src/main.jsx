841736e9d2e6387c525cec831d55a9cab40c9d4b
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-08-06 18:19:05 -0500

    frontend part started

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+
+      <Route path="*" element={<App />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
