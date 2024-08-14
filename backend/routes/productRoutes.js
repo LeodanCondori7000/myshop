5631bc4d5bf370386b7ca6a6f21a02bbcfd653ae
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-08-14 11:48:16 -0500

    deleteProduct controller implemented

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,16 +5,20 @@ import {
   getProductById,
   createProduct,
   updateProduct,
-  // deleteProduct,
+  deleteProduct,
   // createProductReview,
   // getTopProducts,
 } from "../controllers/productController.js";
 
-import { protect, admin } from '../middlewares/authMiddleware.js';
+import { protect, admin } from "../middlewares/authMiddleware.js";
 // import checkObjectId from '../middlewares/checkObjectId.js';
 
-router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 // router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 // router.get('/top', getTopProducts);
