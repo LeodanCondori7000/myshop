445760d365bba73427527ac3999b37cb887dde30
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-08-15 13:39:50 -0500

    OrderScreen component improved

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -63,7 +63,7 @@ const OrderScreen = () => {
       .capture()
       .then(async function (details) {
         try {
-          await payOrder({ orderId, details });
+          await payOrder({ orderId, details }).unwrap();
           refetch();
           toast.success("Order is paid");
         } catch (err) {
